bb8fcf065872848cadd0aa1489fa538554b4e1d4
007MJ <k.mijiza@gmail.com>
2023-05-23 11:59:32 -0400

    end of handling argc

--- main.c
+++ main.c
@@ -6,16 +6,60 @@
 /*   By: mnshimiy <mnshimiy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/20 12:52:26 by mnshimiy          #+#    #+#             */
-/*   Updated: 2023/05/20 16:53:18 by mnshimiy         ###   ########.fr       */
+/*   Updated: 2023/05/22 19:23:52 by mnshimiy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int	main(int argc, char *argv[], char *env[])
+int	size(char *file[], int n)
 {
+	int	i;
+
+	i = 0;
+	while (file[n][i] != '\0')
+		i++;
+	return (n);
+}
+
+char	*fileargv(char *file[], int n)
+{
+	int		i;
+	char	*fil;
+	int		len;
+
+	len = size(file, n);
+	fil = malloc (len * sizeof(char *) + 1);
+	i = 0;
+	while (file[n][i])
+	{
+		fil[i] = file[n][i];
+		i++;
+	}
+	fil[i] = '\0';
+	return (fil);
+}
+
+int	main(int argc, char *argv[])
+{
+	char	*fileone;
+	char	*cmd1;
+	char	*cmd2;
+	char	*filetwo;
+
+	fileone = NULL;
+	cmd1 = NULL;
+	cmd2 = NULL;
+	filetwo = NULL;
 	if (argc <= 5)
 	{
+		fileone = fileargv(argv, 1);
+		cmd1 = fileargv(argv, 2);
+		cmd2 = fileargv(argv, 3);
+		filetwo = fileargv(argv, 4);
 		
 	}
+	else
+		printf("shit don't work !");
+
 }
--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: mnshimiy <mnshimiy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/20 12:38:35 by mnshimiy          #+#    #+#             */
-/*   Updated: 2023/05/20 12:52:19 by mnshimiy         ###   ########.fr       */
+/*   Updated: 2023/05/22 18:48:18 by mnshimiy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,8 @@
 
 # include <stdio.h>
 # include <unistd.h>
-
+# include <stdlib.h>
 
 int	execve(const char *filename, char *const argv[], char *const envp[]);
-
-
-
 
 #endif
